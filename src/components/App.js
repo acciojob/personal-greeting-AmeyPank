c46d1c370a2e5f94cdbb1b314abae4318764496c
Amey  Pankanti <pankantiameya@gmail.com>
2023-05-01 11:59:42 +0530

    submitting code-Mon May 01 2023 11:59:41 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+import React, { useState } from "react";
+function App () {
+  const [name, setName]=useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text"
+        value={name}
+        onChange={(e)=> setName(e.target.value)}
+        />
+        {name? <p>Hello {name}!</p>:null}
     </div>
   )
 }
